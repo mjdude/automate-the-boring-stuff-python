ca92e0b50583f11db1763b238aaec313e2373816
mjdude <mohammed.jalil@gmail.com>
2018-11-25 23:44:31 +0800

    WIP random quiz generator

--- lessons/randomQuizGenerator.py
+++ lessons/randomQuizGenerator.py
@@ -3,7 +3,6 @@
 # random order, along with the answer key
 
 import random
-import 
 
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -25,9 +24,25 @@ for quizNum in range(35):
     # Each quiz , what is the capital of random <state>
     # is it random selection of 4 capitals
     # TODO: Create the quiz and answer key files.
+    quizFile = open('quiz %s.txt' % quizNum, 'a')
+    answerFile = open('answer %s.txt' % quizNum, 'a')
 
     # TODO: Write out the header for the quiz.
+    quizFile.write('Quiz %s ' % quizNum)
 
     # TODO: Shuffle the order of the states.
+    states = list(capitals.keys())
+    random.shuffle(states)
 
     # TODO: Loop through all 50 states, making a question for each.
+    for questionNum in range(50):
+        question = 'What is the capital of ' + states[questionNum]
+    
+    # TODO: Create write and wrong answers for each
+        capitalList = list(capitals.values())
+        
+        
+
+    # TODO: Write the question and answer options to the quiz file.
+
+    # TODO: Write the answer key to a file.
